148ba4e332c2b87e07260a8a62b6cde8c9e1f981
Xavier Henry <53019832+xavier-1-tech@users.noreply.github.com>
2022-01-17 07:54:55 -0400

    Update beanstalk.tf

--- beanstalk.tf
+++ beanstalk.tf
@@ -7,10 +7,9 @@ resource "aws_elastic_beanstalk_application" "eb_app" {
     max_count             = 128
     delete_source_from_s3 = true
   }
-}
-
 
 }
+
 resource "aws_elastic_beanstalk_application_version" "eb_app_ver" {
     bucket = aws_s3_bucket.hemia_bucket.id
     key = aws_s3_bucket_object.eb_bucket_obj.id
