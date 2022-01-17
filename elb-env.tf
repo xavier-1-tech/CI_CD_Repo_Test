8c95b33f52d3e9e15de53d656df01fa2103f03b8
Xavier Henry <53019832+xavier-1-tech@users.noreply.github.com>
2022-01-17 18:02:31 -0400

    Update elb-env.tf

--- elb-env.tf
+++ elb-env.tf
@@ -3,7 +3,7 @@ resource "aws_elastic_beanstalk_environment" "welxome-2-env" {
   name = "welxome-2-tf-env"
   application = aws_elastic_beanstalk_application.welxome-2_app.name
   solution_stack_name = "64bit Amazon Linux 2 v3.3.9 running PHP 8.0"
-  description = "environment for flask app"
+  description = "environment for flask app 2"
   version_label = aws_elastic_beanstalk_application_version.welxome-2_app_ver.name
 
   setting {
