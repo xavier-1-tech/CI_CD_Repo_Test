25ab885966f269c40c43cb6143e6cdeb995a115a
Xavier Henry <53019832+xavier-1-tech@users.noreply.github.com>
2022-01-17 07:54:02 -0400

    Update beanstalk.tf

--- beanstalk.tf
+++ beanstalk.tf
@@ -1,6 +1,14 @@
 resource "aws_elastic_beanstalk_application" "eb_app" {
   name  = "eb-tf-app"
   description = "Hemia-project"
+  
+  appversion_lifecycle {
+    service_role          = aws_iam_role.beanstalk_service.arn
+    max_count             = 128
+    delete_source_from_s3 = true
+  }
+}
+
 
 }
 resource "aws_elastic_beanstalk_application_version" "eb_app_ver" {
