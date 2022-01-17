2555c2b849001afad47dfbee3d8899100368fba8
Xavier Henry <53019832+xavier-1-tech@users.noreply.github.com>
2022-01-17 07:56:31 -0400

    Update beanstalk.tf

--- beanstalk.tf
+++ beanstalk.tf
@@ -1,14 +1,7 @@
 resource "aws_elastic_beanstalk_application" "eb_app" {
-  name  = "eb-tf-app"
+  name  = "hm-tf-app"
   description = "Hemia-project"
   
-  appversion_lifecycle {
-    max_count             = 128
-    delete_source_from_s3 = true
-  }
-
-}
-
 resource "aws_elastic_beanstalk_application_version" "eb_app_ver" {
     bucket = aws_s3_bucket.hemia_bucket.id
     key = aws_s3_bucket_object.eb_bucket_obj.id
